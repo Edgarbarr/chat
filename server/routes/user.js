990063c148c461e24964f2404c298a1f37125373
Edgar Barrientos <edgarbarrientos@Edgars-MBP.lan>
2020-12-09 21:36:15 -0800

    folder strucutre and starting comments

---
+++ server/routes/user.js
@@ -0,0 +1,30 @@
+// routes
+/* 
+//good to do controllers first since we would import that here
+//const controllers = require("path to controllers")
+//need to require express here too
+//dont forget to export routes to use in server file
+good starting routes are 
+a route for /,
+a route for /login,
+a route for /register,
+a route for /:id
+
+*/
+//can make routes here using express Router function
+
+// const router = express.Router();
+/* 
+router  <------ example of a route
+.route("/")
+.get(controllers.getAllUsers)
+.delete(controllers.deleteAllUsers)
+
+router
+.route(/another-route)
+.get(controller.get)
+.post(controller.post)
+.put(controolers.put)
+
+
+*/
