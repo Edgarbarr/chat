b18bb9a73d4999dfb98472f910b1a87df27aa385
Edgar Barrientos <edgarbarrientos@Edgars-MBP.lan>
2020-12-11 12:38:48 -0800

    app socket fix

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,27 +1,12 @@
-import React, {useEffect} from "react"
+import React, { useEffect } from "react";
 import LandingPage from "./views/landing-page";
 import GlobalStyles from "./global.styles";
 import { Route, Switch } from "react-router-dom";
 import DashBoard from "./components/dashboard";
 import socketIOClient from "socket.io-client";
+const io = socketIOClient("http://localhost:3000/");
 const App = () => {
-  useEffect( () =>{
-    const io = socketIOClient("http://localhost:3000/");
-    // (function () {
-      var socket = io();
-      // document.getElementById('form').submit(function (e){
-      //   e.preventDefault();
-      //   socket.emit('chat message', document.getElementById('app').value;
-      //   document.getElementById('app').value()
-      //   return false;
-      // });
-      // socket.on('chat message', function (msg){
-      //   document.getElementById('message').append(getElementById("<li>").test(msg));
-      //   console.log("socket listening");
-      // });
-    socket.emit('connection')
-    //});
-  }, []);
+  useEffect(() => {}, []);
   return (
     <>
       <GlobalStyles />
