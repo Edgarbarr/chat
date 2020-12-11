97d35d7621509bc98089a83a0c06c66057ec404d
sunflail <sunflail2001@yahoo.com>
2020-12-11 09:08:38 -0800

    changed email to required and unique in schema

--- database/models/User.js
+++ database/models/User.js
@@ -14,7 +14,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  email: String,
+  email: { type: String, unique: true, required: true },
 });
 
 const User = mongoose.model("user", userSchema);
