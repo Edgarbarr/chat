6e8e1c3b7a3bc8d3f3b5bbc51d69551e5b4fca8d
Edgar Barrientos <edgarbarrientos@Edgars-MBP.lan>
2020-12-15 09:44:49 -0800

    fixed bad user context changing state

--- client/src/UserContext.js
+++ client/src/UserContext.js
@@ -3,7 +3,11 @@ import React, { useState, createContext, useEffect } from "react";
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState({ username: null, isLoading: true });
+  const [user, setUser] = useState({
+    username: null,
+    isLoading: true,
+    id: null,
+  });
   return (
     <UserContext.Provider value={[user, setUser]}>
       {children}
